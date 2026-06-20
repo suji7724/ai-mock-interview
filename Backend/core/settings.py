376c52dc7a1612537ee68d46398b3c0be8e715c3
Sujit Shah <shahsujit188@gmail.com>
2026-06-20 17:16:42 +0530

    Configure Django SECRET_KEY to load from environment variable

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -43,7 +43,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-r-n406p4*c%-53(2z-clx8dfpx2a$vuwdju-5m^(6$(+uqo=i&"
+SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-r-n406p4*c%-53(2z-clx8dfpx2a$vuwdju-5m^(6$(+uqo=i&")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
